983e53f68236d9d5b66a068392030d9fe96d5f53
Kurt Guardia <kurtguardia@gmail.com>
2020-03-04 13:37:19 +0100

    mobile navigator done, favicon done, everything done except img/videos/code optimization

--- Resources/Js/script.js
+++ Resources/Js/script.js
@@ -107,5 +107,21 @@ $(document).ready(function(){
     }
     );
 
+    /*Mobile nav*/
+    $('.js--nav-icon').click(function(){
+      var nav = $('.js--main-nav');
+      var icon = $('.js--nav-icon i');
+
+      nav.slideToggle(200);
+      if(icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round')
+      } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round')
+      }
+    })
+
+    
 });
 
